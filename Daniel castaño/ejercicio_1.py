674dd8fb036bde449bc131415b176acc7ff4985e
Danniieelcp18 <161090530+Danniieelcp18@users.noreply.github.com>
2024-02-23 18:24:26 -0500

    Add files via upload

---
+++ Daniel castaño/ejercicio_1.py
@@ -0,0 +1,39 @@
+""" tipo datos
+enteros- integer - int """
+# nombre variable -valor
+
+numEntero = 10
+numEntero2 = int(15)
+
+
+""" flotantes =float """
+numFlotante  = 20.55
+numFlotante2 = float(15.36)
+
+""" cadena de caracteres = string =str """
+cadenaTexto  = "esto es un texto"
+cadenaTexto2 = str ("esto es un string")
+
+
+"""boleano = bool"""
+booleano = True
+booleano2 = False
+
+print ("¡hello world!")
+print (numEntero)
+print (numEntero2)
+
+# pasamos a concatenar ( juntar dos o mas tipos de datos)
+# se concatena con (mas) + o con (coma),
+
+print ( "hello " + "world")
+print ( "hello", "world")
+
+print ("hola", numFlotante + numEntero2 , cadenaTexto2)  
+
+variable  = 886522
+
+print (booleano)
+print(type (variable))
+print (booleano2)
+ 
---
+++ Daniel castaño/practica_1.py
@@ -0,0 +1,44 @@
+
+# tipos de variables
+# imput entrada - print salida
+
+
+# nombre = imput ( "ingrese su nombre ")
+#print ("su nombre es :" , nombre) #
+
+varNombr = "Daniel Castaño"
+
+print (varNombr)
+           
+vaAltura = "1.80 cm" 
+
+print (vaAltura)
+
+valPeso = 85
+print (valPeso)
+
+valEdad = (31)
+print (valEdad)
+#print (varPi)
+
+
+""" #  calcular el area de un circulo con un radio dado
+
+varRadcirc = float 
+varPi = float (3.1416)
+varArea = ( varPi * varRadcirc**2)
+
+
+#convertir grados celsius a fahranheit
+
+VarGracel= float
+valGfaht = ((VarGracel * (9/5)) + 32 )
+
+# crear variablea bool para saber si hace sol o no
+       
+print("Buenas tardes esta soleado el dia")
+
+bool = True
+bool2 = False
+
+print ( bool2)"""
