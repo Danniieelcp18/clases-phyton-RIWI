2aa98694a2cfd718886e6b00838cbe2a7a4a91d2
Danniieelcp18 <161090530+Danniieelcp18@users.noreply.github.com>
2024-02-23 21:46:03 -0500

    Add files via upload

--- Daniel castaño/practica_1.py
+++ Daniel castaño/practica_1.py
@@ -1,4 +1,4 @@
-
+"" 
 # tipos de variables
 # imput entrada - print salida
 
@@ -6,39 +6,91 @@
 # nombre = imput ( "ingrese su nombre ")
 #print ("su nombre es :" , nombre) #
 
+ # 1 PUNTO 
 varNombr = "Daniel Castaño"
 
 print (varNombr)
+ 
+#PUNTO 2 
            
-vaAltura = "1.80 cm" 
+vaAltura = float
 
 print (vaAltura)
 
-valPeso = 85
+valPeso = int
 print (valPeso)
 
-valEdad = (31)
+valEdad = int
 print (valEdad)
-#print (varPi)
 
 
-""" #  calcular el area de un circulo con un radio dado
+ #  3  calcular el area de un circulo con un radio dado
 
-varRadcirc = float 
-varPi = float (3.1416)
-varArea = ( varPi * varRadcirc**2)
+varRadcirc = 5
+varPi = 3.1416
+varArea = ( varPi * (varRadcirc**2))
 
+print ("EL AREA DEL CIRCULO ES : ", varArea)
 
-#convertir grados celsius a fahranheit
+#convertir grados celsius a fahranheit """
+""" 
+VarGracel= 85
+valGfaht = (((VarGracel * (9/5)) + 32 ),)
 
-VarGracel= float
-valGfaht = ((VarGracel * (9/5)) + 32 )
+print ( "LOS GRADOS  SON : ", valGfaht, "f")
 
-# crear variablea bool para saber si hace sol o no
+ # 5 crear variablea bool para saber si hace sol o no
        
 print("Buenas tardes esta soleado el dia")
 
-bool = True
-bool2 = False
+boolSol1= True
+boolSol2 = False
 
-print ( bool2)"""
+print (boolSol2)
+
+# 6 concatenar 2 textos para hacer una oracion
+
+A = ("hola mundo")
+B = ("gracias por todo")
+print (A , B)
+ """
+#  7 REALIZAR OPERACIONES ARITMETICAS CON VARIABLES NUMERICAS
+
+""" numero1 = 25
+numero2 = 6
+resultado = numero1 + numero2
+
+print ("la suma es :" , resultado)
+
+resultado = numero1 - numero2
+
+print ("la resta es :" , resultado)
+
+resultado = numero1 + numero2
+
+print ("la multiplicacion es :" , resultado)
+
+resultado = numero1 / numero2
+
+print ("la division es :" , resultado)
+
+resultado = numero1 // numero2
+
+print ("la division entera es :" , resultado) """
+
+
+# 8 CAMBIAR EL VALOR DE UNA VARIABLE E IMPRIMIR SU NUEVO VALOR
+
+""" nombre = "Daniel castaño"
+
+print (nombre)
+
+nombre = "nicolas"
+print (nombre)
+ """
+ 
+ # 9  Utilizar la función input para optener la edad de una variable
+
+edad  = input ("dijite la edad :" )
+print ("su edad  es :" , edad)
+ 
