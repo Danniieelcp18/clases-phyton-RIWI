ae2131fdeee8dc9b2a72451091deea3885054856
Danniieelcp18 <161090530+Danniieelcp18@users.noreply.github.com>
2024-02-23 21:46:39 -0500

    Delete practica_ 3.py

--- practica_ 3.py
+++
@@ -1,6 +0,0 @@
-numEntero = int (input("ingrese numero  "))
-numEntero1 =int (input("ingrese numero  ")) 
-
-print  ("la suma es: " , numEntero+numEntero1)
-
-print ( "la resta es : ",numEntero1- numEntero)
